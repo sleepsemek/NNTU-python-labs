fc59a68d6cb27de3fddf46a87b3fc8b14f3341d8
Даниил <49837194+sleepsemek@users.noreply.github.com>
2026-05-27 01:16:12 +0300

    Add audio analysis script with plotting features
    This script analyzes an audio file, providing information such as sample rate, total samples, and duration. It also plots discrete samples, an oscillogram, the imaginary part of the FFT, and a histogram of the signal.

---
+++ lab2/lab2.py
@@ -0,0 +1,94 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.io.wavfile as wavfile
+
+def main():
+    file_name = "23.wav"
+
+    if not os.path.exists(file_name):
+        print(f"Файл {file_name} не найден")
+        return
+
+    try:
+        sample_rate, data = wavfile.read(file_name)
+    except:
+        print(f"Не удалось прочитать файл")
+        return
+
+    print(f"Файл '{file_name}' успешно загружен, частота дискретизации: {sample_rate} Гц")
+
+    #Если стерео, достаем первый канал
+    if len(data.shape) > 1:
+        print("Будет использован только первый канал")
+        data = data[:, 0]
+
+    total_samples = len(data)
+    duration_sec = total_samples / sample_rate
+    print(f"Всего отсчетов: {total_samples}")
+    print(f"Длительность записи: {duration_sec:.3f} секунд")
+
+    #Ввод количества отсчетов
+    while True:
+        try:
+            num_samples = int(input(f"\nВведите количество отсчетов для графика 1 (от 1 до {total_samples}): "))
+            
+            if 1 <= num_samples <= total_samples:
+                break
+            else:
+                print(f"Введите число в нужном диапазоне")
+        except ValueError:
+            print("Введите целое число")
+
+    #Настройка плота для 4 графиков
+    plt.figure(figsize=(14, 10))
+    plt.suptitle(f"Анализ аудиосигнала файла {file_name}")
+
+    #График дискретных отсчетов
+    plt.subplot(2, 2, 1)
+    samples_to_plot = data[:num_samples]
+    x_indices = np.arange(num_samples)
+    plt.plot(x_indices, samples_to_plot, linestyle='-', marker='o')
+    plt.title('1.1 Дискретные отсчеты сигнала')
+    plt.xlabel('Номер отсчета')
+    plt.ylabel('Амплитуда')
+    plt.grid(True)
+
+    #Осциллограмма как функция времени
+    plt.subplot(2, 2, 2)
+    time_axis = np.arange(total_samples) / sample_rate
+    plt.plot(time_axis, data)
+    plt.title('1.2 Осциллограмма сигнала')
+    plt.xlabel('Время (секунды)')
+    plt.ylabel('Амплитуда')
+    plt.grid(True)
+
+    plt.subplot(2, 2, 3)
+    #Выполняем ДПФ
+    fft_result = np.fft.fft(data)
+    #Достаем мнимую часть
+    imag_fft = np.imag(fft_result)
+    #Получаем массив соответствующих частот
+    freqs = np.fft.fftfreq(total_samples, d=1/sample_rate)
+    
+    #Строим только положительные частоты
+    half_n = total_samples // 2
+    plt.plot(freqs[:half_n], imag_fft[:half_n])
+    plt.title('1.3 Спектр (Мнимая часть ДПФ)')
+    plt.xlabel('Частота Гц')
+    plt.ylabel('Мнимая часть')
+    plt.grid(True)
+
+    #Гистограмма отсчетов
+    plt.subplot(2, 2, 4)
+    plt.hist(data, bins=100) #второй аргумент - колво интервалов группировки амплитуд
+    plt.title('1.4 Гистограмма отсчетов сигнала')
+    plt.xlabel('Амплитуда')
+    plt.ylabel('Количество попаданий')
+    plt.grid(True)
+
+    print("Закройте окно для завершения работы")
+    plt.show()
+
+if __name__ == "__main__":
+    main()
